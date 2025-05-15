dc4768d025b7d309b961556a2f461203b7263dfa
KoenMW <89910070+KoenMW@users.noreply.github.com>
2025-05-15 13:41:52 +0200

    feat: bad donut implementation

--- src/components/RunDonutChart.svelte
+++ src/components/RunDonutChart.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import * as d3 from "d3";
+  import { on } from "svelte/events";
+  import { drawDonut } from "../core/d3";
+  import type { derived } from "svelte/store";
+  import { onMount } from "svelte";
 
   type props = {
     onTarget: number;
@@ -9,13 +13,28 @@
 
   const { onTarget, nearTarget, underperformed }: props = $props();
 
-  const pie = d3.pie();
-  const arcs = pie([onTarget, nearTarget, underperformed]);
+  let donut: HTMLDivElement;
 
-  const svg = d3.select("#donut");
+  onMount(() => {
+    const { svg, change } = drawDonut([
+      { apples: onTarget, oranges: 200 },
+      { apples: nearTarget, oranges: 200 },
+      { apples: underperformed, oranges: 200 },
+    ]);
 
-  svg.append("title").text("test");
-  console.log("donut:", svg);
+    const chart = svg.node();
+    if (chart) donut.appendChild(chart);
+
+    let test = true;
+
+    setInterval(() => {
+      change(test ? "apples" : "oranges");
+      test = !test;
+    }, 2000);
+  });
 </script>
 
-<h1>donut chart 🍩</h1>
+<h1>
+  donut chart 🍩
+  <div class="donut-container" bind:this={donut}></div>
+</h1>
